109663bad3b68135f5239315d4c3c6bf27c6a114
Dane Johnson <dane_johnson@ymail.com>
2016-06-07 12:52:06 -0500

    added menu

--- TimeCouchRPG/source/MenuState.hx
+++ TimeCouchRPG/source/MenuState.hx
@@ -9,13 +9,30 @@ import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
+	private var titleTxt: FlxText;
+	private var descTxt: FlxText;
+	private var goBtn: FlxButton;
 	override public function create():Void
 	{
+		titleTxt = new FlxText(0, 0, 0, "Time Couch RPG", 12);
+		add(titleTxt);
+		
+		descTxt = new FlxText(0, 20, 0, "Prototype version 1.0 by Dane Johnson", 8);
+		add(descTxt);
+		
+		goBtn = new FlxButton(0, 40, "Go!", clickGo);
+		add(goBtn);
+		
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+	}
+	
+	private function clickGo()
+	{
+		FlxG.switchState(new PlayState());
 	}
 }
